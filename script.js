d5eae27e374bffcdfbbb1da961a4bf30e3643258
ayatsakalla <144719331+ayatsakalla@users.noreply.github.com>
2024-04-12 00:18:40 +0000

    adding fonts and styling, and added the triggering enter feature in javascript for tha addNameBtn

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ function addName() {
   const nameInput = document.querySelector("#nameInput");
   const name = nameInput.value.trim();
   nameArray.push(name);
+
   displayNames()
   nameInput.value = "";
 }
 
+// displays names entered in the input box
 function displayNames() {
     const nameList = document.querySelector("#nameList")// get UL element
     nameList.innerHTML = '';
@@ -25,6 +27,7 @@ function displayNames() {
     }
 }
 
+// picks random name from the list and displays it
 function pickRandomName() {
     const randomNameDiv = document.querySelector('#randomName');
     randomNameDiv.textContent = '';
@@ -34,11 +37,27 @@ function pickRandomName() {
 
     randomNameDiv.textContent = randomName;
 
+    // styling the selected name in red and bigger font and style
+    randomNameDiv.classList.add("bloodred");
+    randomNameDiv.classList.add("display-2");
+    randomNameDiv.classList.add("miology");
+
     nameArray.splice(randomNumber, 1);
 
     displayNames();
 }
 
+// when pressing enter, it will trigger to click the button when entering list of names
+nameInput.addEventListener("keypress", function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      // Trigger the button element with a click
+      document.getElementById("addNameBtn").click();
+    }
+  });
+
+// references the button to click on to enter the names in the list
 document.querySelector("#addNameBtn").addEventListener("click", addName);
 
+// references the button to click on to pick a random name from the list of names
 document.querySelector("#pickRandomBtn").addEventListener("click", pickRandomName);
